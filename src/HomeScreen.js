42df23dfb2ab99d3519615bf0665a607b69a6b01
ph5500 <philfives@gmail.com>
2021-06-16 15:03:45 -0600

    mapped through movie categories and styled movie rows. Need to align remaining categories with correct api request

--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -1,6 +1,8 @@
 import React from "react";
 import Nav from "../src/Nav";
 import Banner from "../src/Banner";
+import requests from "./Requests";
+import Row from "./Row";
 
 function HomeScreen() {
   return (
@@ -9,7 +11,38 @@ function HomeScreen() {
 
       <Banner />
 
-      {/* Rows */}
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow
+      />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} isLargeRow />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} isLargeRow />
+      <Row
+        title="Action Movies"
+        fetchUrl={requests.fetchActionMovies}
+        isLargeRow
+      />
+      <Row
+        title="Comedy Movies"
+        fetchUrl={requests.fetchComedyMovies}
+        isLargeRow
+      />
+      <Row
+        title="Horror Movies"
+        fetchUrl={requests.fetchHorrorMovies}
+        isLargeRow
+      />
+      <Row
+        title="Romance Movies"
+        fetchUrl={requests.fetchRomanceMovies}
+        isLargeRow
+      />
+      <Row
+        title="Documenteries"
+        fetchUrl={requests.fetchDocumentaries}
+        isLargeRow
+      />
     </header>
   );
 }
